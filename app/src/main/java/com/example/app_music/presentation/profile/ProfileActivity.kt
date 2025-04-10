00504dd13e41f8817247b44b6d00e9290be0bb68
maybetuandat <trinhvinhtuandat05102003@gmail.com>
2025-04-10 16:17:04 +0700

    update profile activity and setting activity

---
+++ app/src/main/java/com/example/app_music/presentation/profile/ProfileActivity.kt
@@ -0,0 +1,23 @@
+package com.example.app_music.presentation.profile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.app_music.R
+import com.example.app_music.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnBack.setOnClickListener{
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+    }
+}
