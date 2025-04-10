338a3ca0cb5bc2dec2c7f1b16e555491ec2441a1
maybetuandat <trinhvinhtuandat05102003@gmail.com>
2025-04-10 21:56:49 +0700

    add edit username activity, edit status activity, edit student information activity

---
+++ app/src/main/java/com/example/app_music/presentation/editStatusMessage/EditStatusMessageActivity.kt
@@ -0,0 +1,17 @@
+package com.example.app_music.presentation.editStatusMessage
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.app_music.R
+
+class EditStatusMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_status_message)
+
+    }
+}
--- app/src/main/java/com/example/app_music/presentation/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/app_music/presentation/profile/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.example.app_music.presentation.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.app_music.R
 import com.example.app_music.databinding.ActivityProfileBinding
+import com.example.app_music.presentation.editStatusMessage.EditStatusMessageActivity
+import com.example.app_music.presentation.editStudentInformation.EditStudentInformation
+import com.example.app_music.presentation.editUserNameScreen.EditUserNameActivity
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding : ActivityProfileBinding
@@ -17,6 +21,18 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.btnBack.setOnClickListener{
             onBackPressedDispatcher.onBackPressed()
+        }
+        binding.flUsernameContainer.setOnClickListener {
+            val intent = Intent(this, EditUserNameActivity::class.java)
+            startActivity(intent)
+        }
+        binding.flStatusContainer.setOnClickListener {
+            val intent = Intent(this, EditStatusMessageActivity::class.java)
+            startActivity(intent)
+        }
+        binding.flStudentInfoContainer.setOnClickListener {
+            val intent = Intent(this, EditStudentInformation::class.java)
+            startActivity(intent)
         }
 
     }
