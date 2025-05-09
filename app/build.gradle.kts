571e1689a82dcb4528a2679699cd7d34b81793ef
maybetuandat <trinhvinhtuandat05102003@gmail.com>
2025-05-09 22:50:27 +0700

    add api instance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation ("com.google.android.material:material:1.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
 }
