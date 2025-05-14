a7a1edaf02313ec7dda57e624aabd593fb7dfb0a
Ha Cuong Thinh <thinhhacuong123@gmail.com>
2025-05-14 12:42:05 +0700

    fix localdate conflict

--- app/src/main/java/com/example/app_music/domain/utils/RetrofitInstance.kt
+++ app/src/main/java/com/example/app_music/domain/utils/RetrofitInstance.kt
@@ -1,14 +1,20 @@
 package com.example.app_music.domain.utils
 
 import com.example.app_music.data.remote.api.ApiInterface
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.time.LocalDate
 
 object RetrofitInstance {
     val api: ApiInterface by lazy {
+        val gson = GsonBuilder()
+            .registerTypeAdapter(LocalDate::class.java, LocalDateTypeAdapter())
+            .create()
+
         Retrofit.Builder()
             .baseUrl(ApiInstance.baseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiInterface::class.java)
     }
