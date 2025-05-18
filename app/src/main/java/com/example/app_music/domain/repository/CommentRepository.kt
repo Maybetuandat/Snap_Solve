d0ddb50b1bbe764c49e8cca0c723d0786f33cc35
Ha Cuong Thinh <thinhhacuong123@gmail.com>
2025-05-18 18:53:58 +0700

    add logic for comment

---
+++ app/src/main/java/com/example/app_music/domain/repository/CommentRepository.kt
@@ -0,0 +1,4 @@
+package com.example.app_music.domain.repository
+
+interface CommentRepository {
+}
---
+++ app/src/main/java/com/example/app_music/presentation/feature/community/communityPostDetail/CommentReplyViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.app_music.presentation.feature.community.communityPostDetail
+
+class CommentReplyViewModel {
+}
