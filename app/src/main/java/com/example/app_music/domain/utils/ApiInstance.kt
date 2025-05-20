499849bf35cf63e4c9bb70f7c3ee2bafe6026bb7
01693334945quan@gmail.com <nguyenanhquan.team5itptit@gmail.com>
2025-05-20 15:55:06 +0700

    update

--- app/src/main/java/com/example/app_music/domain/utils/ApiInstance.kt
+++ app/src/main/java/com/example/app_music/domain/utils/ApiInstance.kt
@@ -1,5 +1,5 @@
 package com.example.app_music.domain.utils
 
 object ApiInstance {
-        const val baseUrl = "http://192.168.102.9:9999"
+        const val baseUrl = "http://192.168.2.12:9999"
 }
